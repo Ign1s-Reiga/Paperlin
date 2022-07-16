6b253edec4aa0429cd4462a9b8a58d566e6ae8be
Reiga <remrem4862@gmail.com>
2022-07-16 22:51:08 +0900

    add line

--- src/main/kotlin/dev/reiga7953/spikotlin/Schedule.kt
+++ src/main/kotlin/dev/reiga7953/spikotlin/Schedule.kt
@@ -8,24 +8,24 @@ fun JavaPlugin.cancelTasks() = server.scheduler.cancelTasks(this)
 
 fun JavaPlugin.schedule(
     async: Boolean = false,
-    delay: Long = 0,
-    period: Long = 0,
+    sdelay: Long = 0,
+    speriod: Long = 0,
     unit: TimeUnit = TimeUnit.SECONDS,
     callback: BukkitTask.() -> Unit
 ): BukkitTask = let { plugin ->
     lateinit var task: BukkitTask
     fun f() = task.callback()
     task = server.scheduler.run {
-        val sdelay = unit.toSeconds(delay) * 20
-        val speriod = unit.toSeconds(period) * 20
+        val sdelay = unit.toSeconds(sdelay) * 20
+        val speriod = unit.toSeconds(speriod) * 20
         when {
-            period > 0 -> {
+            speriod > 0 -> {
                 when {
                     async -> runTaskTimerAsynchronously(plugin, ::f, sdelay, speriod)
                     else -> runTaskTimer(plugin, ::f, sdelay, speriod)
                 }
             }
-            delay > 0 -> {
+            sdelay > 0 -> {
                 when {
                     async -> runTaskLaterAsynchronously(plugin, ::f, sdelay)
                     else -> runTaskLater(plugin, ::f, sdelay)
